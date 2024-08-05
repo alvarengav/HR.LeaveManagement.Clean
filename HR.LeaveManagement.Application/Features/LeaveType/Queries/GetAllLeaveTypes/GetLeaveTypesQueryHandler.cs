6f3cf528fca129c5e8e632600c3991a9183e10d8
Luis Alvarenga <l.alvarengav@gmail.com>
2024-08-05 09:53:12 -0600

    Add project files.

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Logging;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+public class GetLeaveTypesQueryHandler : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
+{
+    public readonly IMapper _mapper;
+    public readonly ILeaveTypeRepository _leaveTypeRepository;
+    private readonly IAppLogger<GetLeaveTypesQueryHandler> _logger;
+
+    public GetLeaveTypesQueryHandler(
+        IMapper mapper,
+        ILeaveTypeRepository leaveTypeRepository,
+        IAppLogger<GetLeaveTypesQueryHandler> logger
+    )
+    {
+        _mapper = mapper;
+        _leaveTypeRepository = leaveTypeRepository;
+        _logger = logger;
+    }
+
+    public async Task<List<LeaveTypeDto>> Handle(
+        GetLeaveTypesQuery request,
+        CancellationToken cancellationToken
+    )
+    {
+        var leaveTypes = await _leaveTypeRepository.GetAsync();
+
+        var data = _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
+
+        _logger.LogInformation("Leave types where retrieved successfully");
+
+        return data;
+    }
+}
